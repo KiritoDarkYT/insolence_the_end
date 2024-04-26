a45eb7b08505cf88db79f2f94648758ed8d03de3
unknown <lukasirado17@gmail.com>
2024-04-26 20:34:08 -0300

    Initial commit

---
+++ src/main/java/net/insolencesmp/theend/Insolence.java
@@ -0,0 +1,46 @@
+package net.insolencesmp.theend;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Insolence.InsolenceEnd)
+public class Insolence
+{
+    public static final String InsolenceEnd = "theend_ismp";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Insolence() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @Mod.EventBusSubscriber(modid = InsolenceEnd, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
